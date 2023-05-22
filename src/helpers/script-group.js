607650e153f5497bbb27375bbdb271ceffd232de
Breno Battaglin <breno.battaglin@icloud.com>
2023-05-22 19:53:28 -0300

    refactor: Create script group helper

---
+++ src/helpers/script-group.js
@@ -0,0 +1,9 @@
+export default class ScriptGroupHelper {
+  static createGroup(name) {
+    return {
+      name: name,
+      value: "",
+      disabled: "-",
+    };
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import select, { Separator } from "@inquirer/select";
 import runFirefoxEpicGames from "./scripts/firefox-epic-games.js";
 import runFirefoxPrimeGaming from "./scripts/firefox-prime-gaming.js";
+import ScriptGroupHelper from "./helpers/script-group.js";
 
-const ScriptGroups = Object.freeze({
-  FIREFOX: "Firefox",
-  FREE_GAMES: "Get free games",
-});
+class ScriptGroups {
+  static FIREFOX = "Firefox";
+  static OBSIDIAN = "Obsidian";
+}
+
+class ScriptNames {
+  static FIREFOX_FREE_GAMES = "Get free games";
+  static OBSIDIAN_DAILY_NOTES = "Create daily notes";
+  static OBSIDIAN_WEEKLY_NOTES = "Create weekly notes";
+}
 
 const ScriptKeys = Object.freeze({
   FIREFOX_FREE_GAMES: "firefox-free-games",
@@ -32,13 +39,9 @@ console.log(mortyAscii);
 const answer = await select({
   message: "Select the action",
   choices: [
+    ScriptGroupHelper.createGroup(ScriptGroups.FIREFOX),
     {
-      name: ScriptGroups.FIREFOX,
-      value: "",
-      disabled: "-",
-    },
-    {
-      name: ScriptGroups.FREE_GAMES,
+      name: ScriptNames.FIREFOX_FREE_GAMES,
       value: ScriptKeys.FIREFOX_FREE_GAMES,
       description: "Open Epic Games' and Amazon Prime's free games sections",
     },
